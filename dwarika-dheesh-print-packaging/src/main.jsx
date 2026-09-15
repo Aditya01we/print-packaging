f6a95b704963e05fc642cd50fa646d9cd2465d2c
Aditya Bhardwaj <adityabhardwajdpr@gmail.com>
2026-09-15 23:02:09 +0530

    updated ui

--- dwarika-dheesh-print-packaging/src/main.jsx
+++ dwarika-dheesh-print-packaging/src/main.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/professional-theme.css";
 
 const rootElement = document.getElementById("root");
 
